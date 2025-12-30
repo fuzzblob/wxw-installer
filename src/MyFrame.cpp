b11b24351f70174d9de6f12bf19e21604333f4cf
fuzzblob <admin@fuzzblob.com>
2025-12-30 17:18:27 +0100

    create vertical layout
    - find wxVERTICAL (vs wx.VERTICAL referenced in example)

--- src/MyFrame.cpp
+++ src/MyFrame.cpp
@@ -1,4 +1,5 @@
 #include "MyFrame.h"
+#include <wx/wx.h>
 
 namespace wxInstaller
 {
@@ -10,5 +11,15 @@ namespace wxInstaller
 
         // Create a button
         wxButton* button = new wxButton(panel, wxID_ANY, "Click Me!", wxDefaultPosition, wxSize(100, 30));
+
+        // Create a sizer to manage the layout
+        wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+        sizer->Add(button, 0, wxALIGN_CENTER | wxALL, 10);
+
+        // Set the sizer for the panel
+        panel->SetSizer(sizer);
+
+        // Set the size of the frame
+        this->SetSize(300, 200);
     };
 };
