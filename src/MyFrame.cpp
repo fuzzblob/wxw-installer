18835210e7269684355082586f0772ee68a0b7fd
fuzzblob <admin@fuzzblob.com>
2025-12-30 16:59:42 +0100

    add a test button

--- src/MyFrame.cpp
+++ src/MyFrame.cpp
@@ -4,5 +4,11 @@ namespace wxInstaller
 {
     MyFrame::MyFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
         // Initialize components here
+
+        // Create a panel to hold the widgets
+        wxPanel* panel = new wxPanel(this);
+
+        // Create a button
+        wxButton* button = new wxButton(panel, wxID_ANY, "Click Me!", wxDefaultPosition, wxSize(100, 30));
     };
 };
